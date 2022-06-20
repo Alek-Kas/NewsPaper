178673beaf74ac55e013a7ddea62ad4e240cf6ec
Aleks-Kas <spawn_kas@mail.ru>
2022-06-20 15:27:22 +0300

    0.003 Модели дописаныб нужно дописать методы

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -5,33 +5,20 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):  # наследуемся от класса Model
     rating = models.IntegerField(default=0)
-    staff = models.OneToOneField(User, on_delete=models.CASCADE())
+    staff = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
-# weather = 'WE'
-# sports = 'SP'
-# politics = 'PO'
-# education = 'ED'
-# incidents = 'IN'
-# economy = 'EC'
+    #  Метод update_rating() модели Author, который обновляет рейтинг пользователя, переданный в аргумент этого метода
+    def update_rating(self):
+        pass
 
 
 class Category(models.Model):
-    # CAT = [
-    #     (weather, 'Погода'),
-    #     (sports, 'Спорт'),
-    #     (politics, 'Политика'),
-    #     (education, 'Образование'),
-    #     (incidents, 'Проишествия'),
-    #     (economy, 'Экономика')
-    # ]
-    cat = models.CharField(max_length=64,
-                           # choices=CAT,
-                           # default=weather,
-                           unique=True)
+    cat = models.CharField(max_length=64, unique=True)
+
 
 article = 'AR'
 news = 'NE'
+
 
 class Post(models.Model):
     POST = [
@@ -41,22 +28,40 @@ class Post(models.Model):
     post = models.CharField(max_length=2,
                             choices=POST,
                             default=article)
-    post_time = models.DateTimeField()
+    post_time = models.DateTimeField(auto_now_add=True)
     post_heading = models.CharField(max_length=64)
-    post_text = models.CharField(max_length=1024)
+    post_text = models.TextField(max_length=1024)
     post_rating = models.IntegerField(default=0)
-    post_author = models.ForeignKey(Author, on_delete=models.CASCADE())
-    post_cat = models.ManyToManyField(PostCategory)
+    post_author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    post_cat = models.ManyToManyField('Category', through='PostCategory')
+
+    #  Методы like() и dislike() в моделях Comment и Post, которые увеличивают/уменьшают рейтинг на единицу
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
+
+    #  Метод preview() модели Post, который возвращает начало статьи (предварительный просмотр) длиной 124 символа и добавляет многоточие в конце.
+    def preview(self):
+        pass
 
 
 class PostCategory(models.Model):
-    post_cat_post = models.ManyToManyField(Post)
-    post_cat_cat = models.ManyToManyField(Category)
+    post_cat_post = models.ForeignKey('Post')
+    post_cat_cat = models.ForeignKey('Category')
 
 
 class Comment(models.Model):
     comment_text = models.CharField(max_length=128)
-    comment_time = models.DateTimeField()
+    comment_time = models.DateTimeField(auto_now_add=True)
     comment_rating = models.IntegerField(default=0)
-    comment_post = models.ForeignKey(Post)
-    comment_user = models.ForeignKey(User)
+    comment_post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    comment_user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    #  Методы like() и dislike() в моделях Comment и Post, которые увеличивают/уменьшают рейтинг на единицу
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
