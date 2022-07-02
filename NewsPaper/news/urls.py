6e6e780c7a2fb98a482968f1f723c63afe7add36
Alek-Kas <spawn_kas@mail.ru>
2022-07-02 20:35:40 +0300

    1.4 Сделано: Третий пункта из задания

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from .views import ProductsList, ProductDetail
-from .views import NewsList
+from .views import NewsList, NewsDetail
 
 urlpatterns = [
    # path('', ProductsList.as_view()),
    # path('<int:pk>', ProductDetail.as_view()),
-    path('', NewsList.as_view())
+    path('', NewsList.as_view()),
+    path('<int:pk>', NewsDetail.as_view()),
 ]
