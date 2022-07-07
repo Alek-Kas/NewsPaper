796359d6fa153f951a3fb0d6118c2ca4dd2ae7b4
Alek-Kas <spawn_kas@mail.ru>
2022-07-08 00:04:30 +0300

    3.13 Надо: 2) Добавьте страницу /news/search. На ней должна быть реализована возможность искать новости по определённым критериям. Критерии должны быть следующие: по названию; по тегу; позже указываемой даты.
    Уже отображается на странице фильтры, но пока не работают, теперь работают, но нужно сделать выбор даты - пока отложу и займусь пунктом 4

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     path('', NewsList.as_view(), name='newslist'),
     path('<int:pk>', NewsDetail.as_view(), name='news'),
     path('search/', NewsSearch.as_view(), name='newssearch'),
+    '''
+    /news/create/
+    /news/<int:pk>/edit/
+    /news/<int:pk>/delete/
+    /articles/create/
+    /articles/<int:pk>/edit/
+    /articles/<int:pk>/delete/
+    '''
 ]
