69233c3570d8c31584b10dcae20fdc9034667677
Alek-Kas <spawn_kas@mail.ru>
2022-07-20 21:17:13 +0300

    4.7 Сделано - 9) Создать возможность стать автором (быть добавленным в группу authors).

--- NewsPaper/sign/views.py
+++ NewsPaper/sign/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.views.generic.edit import CreateView
 from .models import BaseRegisterForm
 
@@ -9,3 +10,11 @@ class BaseRegisterView(CreateView):
     model = User
     form_class = BaseRegisterForm
     success_url = '/'
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
