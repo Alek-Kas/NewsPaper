c7d5037eb1a119fb5f0761ddb6627f0ac37aaf4b
Alek-Kas <spawn_kas@mail.ru>
2022-07-20 20:24:56 +0300

    4.5 Реализовал регистрацию и авторизацию через GitHub

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     # ... include the providers you want to enable:
-    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
 ]
 
 MIDDLEWARE = [
