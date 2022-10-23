535b263226f85109f5b477a09b137c0bbddba3ae
Alek-Kas <spawn_kas@mail.ru>
2022-10-23 13:05:58 +0300

    6.13 - Сделано еженедельная рассылка почты в понедельник в 8 утра через Celery, Redis and other

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -13,8 +13,8 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'mailing_every_monday_8am': {
         'task': 'news.tasks.mailing_m_8am',
-        'schedule': crontab(minute=0, hour='*'),
-        # 'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+        # 'schedule': crontab(minute=0, hour='*'),
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
         # 'args': (agrs),
     },
     # 'test_print': {
